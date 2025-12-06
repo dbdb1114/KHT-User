d9551b0a94f332ec3dace8e3e11d18924b86b18b
유정현 <dbdb1114@naver.com>
2025-12-06 21:41:52 +0900

    Runner commit

---
+++ src/main/java/com/kht/user/KoreaHistoryTestUserApplication.java
@@ -0,0 +1,13 @@
+package com.kht.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaHistoryTestUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaHistoryTestUserApplication.class, args);
+	}
+
+}
